4beb0ad3b78f4b39f07e4dbdb26d467d6efa4cb6
Deepwinter <deepwinter@winterroot.net>
2013-03-19 19:57:14 -0700

    Basic android app with a google map view fragment

---
+++ src/org/calflora/observer/MapOverview.java
@@ -0,0 +1,111 @@
+package org.calflora.observer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.MapFragment;
+
+public class MapOverview extends Activity implements LocationListener {
+
+	
+	private GoogleMap map;
+	private static final LatLng ROMA = new LatLng(42.093230818037,11.7971813678741);
+	private LocationManager locationManager;
+	private String provider;
+	
+	public MapOverview() {
+		super();
+		/*
+		this.mContext = mContext;
+	    String context=Context.LOCATION_SERVICE;
+	    lm = (LocationManager)mContext.getSystemService(context);
+	    lm.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER,MINIMUM_TIME_BETWEEN_UPDATES,MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,this);
+*/
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map_overview);
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		map.setMapType(1);
+		map.setMyLocationEnabled(true);
+		
+		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+	    boolean enabledGPS = service
+	                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+	    boolean enabledWiFi = service
+	                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		
+		 // Check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to 
+        // go to the settings
+        if (!enabledGPS) {
+            Toast.makeText(this, "GPS signal not found", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+        
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        
+        // Initialize the location fields
+        if (location != null) {
+            Toast.makeText(this, "Selected Provider " + provider,
+                    Toast.LENGTH_SHORT).show();
+            onLocationChanged(location);
+        } else {
+
+            //do something
+        }
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.map_overview, menu);
+		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
