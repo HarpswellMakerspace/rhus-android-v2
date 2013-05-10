2fab9b99fbdcc94cb4c00a7b3c9f330777531b1d
Deepwinter <deepwinter@winterroot.net>
2013-05-10 00:38:04 -0700

    Build Number 28

--- tools/upload.sh
+++ tools/upload.sh
@@ -1,5 +1,5 @@
 curl http://testflightapp.com/api/builds.json --progress-bar \
- -F file=@bin/Calflora.apk \
+ -F file=@bin/Calflora-release.apk \
  -F api_token='c78a3623160611823a1fe0613bc3e678_Nzg4Mjk3MjAxMi0xMi0xNCAxNDoyMzo1NC43Nzk3NjY' \
  -F team_token='e48a64f95f61c4e02fc08fe6d125bba3_MjExMDgxMjAxMy0wNC0xMiAxNzozNjoyMS4xNzE5NzE' \
  -F notes="$1" \
